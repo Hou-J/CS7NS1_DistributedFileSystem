5c098e2c2674bc00792c2ed69654d940bf1ea339
jhou <jiongxuhou@gmail.com>
2017-12-14 22:17:03 +0000

    failed caching, try later

--- Caching/client.py
+++ Caching/client.py
@@ -8,7 +8,7 @@ if (len(sys.argv) < 3):
 if __name__ == "__main__":
 
     address = "{}:{}".format(sys.argv[1], int(sys.argv[2]))
-    local_catch = []
+    local_cache = []
 
     while True:
         actionNum = int(input(
@@ -25,32 +25,32 @@ if __name__ == "__main__":
             "---------------------------------------\n"))
 
         if actionNum == 1:
-            clientLibrary.fileLists(clientLibrary, address, local_catch)
+            clientLibrary.fileLists(clientLibrary, address, local_cache)
 
         elif actionNum == 2:
             fileToOpen = input("Input the complete filename you want to open: ")
-            clientLibrary.readFile(clientLibrary, address, fileToOpen, local_catch)
+            clientLibrary.readFile(clientLibrary, address, fileToOpen, local_cache)
 
         elif actionNum == 3:
             fileToAdd = input("Input the complete filename you want to add: ")
             fileData = input("Input what you want to add to the {}: ".format(fileToAdd))
-            clientLibrary.addFile(clientLibrary, address, fileToAdd, fileData, local_catch)
+            clientLibrary.addFile(clientLibrary, address, fileToAdd, fileData, local_cache)
 
         elif actionNum == 4:
             fileToAdd = input("Input the complete filename you want to edit: ")
             fileData = input("Input the new data in {}: ".format(fileToAdd))
-            clientLibrary.editFile(clientLibrary, address, fileToAdd, fileData, local_catch)
+            clientLibrary.editFile(clientLibrary, address, fileToAdd, fileData, local_cache)
 
         elif actionNum == 5:
             fileToDelete = input("Input the complete filename you want to delete: ")
             yn = input("Are you sure you want to delete {} ? (Y/N)".format(fileToDelete))
             if yn == "n" or yn == "N":
                 continue
-            clientLibrary.deleteFile(clientLibrary, address, fileToDelete, local_catch)
+            clientLibrary.deleteFile(clientLibrary, address, fileToDelete, local_cache)
 
         elif actionNum == 6:
             fileToPush = input("Input the complete filename you want to push: ")
-            clientLibrary.pushFile(clientLibrary, address, fileToPush, local_catch)
+            clientLibrary.pushFile(clientLibrary, address, fileToPush, local_cache)
 
         elif actionNum == 0:
             break
--- Caching/clientlibrary.py
+++ Caching/clientlibrary.py
@@ -2,7 +2,7 @@ import requests, json
 
 
 class clientLibrary():
-    def fileLists(self, address, local_catch):
+    def fileLists(self, address, local_cache):
         r = requests.get("http://{}/fileList".format(address))
         filelists = json.loads(r.text)
 
@@ -10,20 +10,30 @@ class clientLibrary():
         print("File List:\n")
         for filename in filelists:
             print(filename)
+
+        print("Catch List:\n")
+        for l in local_cache:
+            print(l['filename'])
         print("\n-------------action end----------------")
 
-    def readFile(self, address, fileName, local_catch):
-        r = requests.get("http://{}/file/{}".format(address, fileName))
-        data = json.loads(r.text)
-        if data == False:
-            print("File do not exit!")
+    def readFile(self, address, fileName, local_cache):
+        l  = [l for l in local_cache if l['filename'] == fileName]
+        if len(l) == 0:
+            print("File not in cache.")
+            r = requests.get("http://{}/file/{}".format(address, fileName))
+            data = json.loads(r.text)
+            if data == False:
+                print("File do not exit!")
+            else:
+                local_cache.append(data)
         else:
+            print("Found file in cache.")
             print("--------------file start-----------------")
-            for d in data:
+            for d in l:
                 print(d, end='')
             print("\n---------------file end------------------")
 
-    def addFile(self, address, fileName, data, local_catch):
+    def addFile(self, address, fileName, data, local_cache):
         r = requests.post("http://{}/fileList".format(address), json={'fileName': fileName, 'data': data})
         data = json.loads(r.text)
         if data == False:
@@ -34,7 +44,7 @@ class clientLibrary():
                 print(d, end='')
             print("\n---------------file end------------------")
 
-    def editFile(self, address, FileName, data, local_catch):
+    def editFile(self, address, FileName, data, local_cache):
         r = requests.put("http://{}/file/{}".format(address, FileName), json={'data': data})
         data = json.loads(r.text)
         if data == False:
@@ -45,7 +55,7 @@ class clientLibrary():
                 print(d, end='')
             print("\n---------------file end------------------")
 
-    def deleteFile(self, address, fileName, local_catch):
+    def deleteFile(self, address, fileName, local_cache):
         r = requests.delete("http://{}/file/{}".format(address, fileName))
         data = json.loads(r.text)
         if data == False:
@@ -53,5 +63,5 @@ class clientLibrary():
         elif data == True:
             print("-------------file deleted----------------")
 
-    def pushFile(self, address, fileToPush, local_catch):
+    def pushFile(self, address, fileToPush, local_cache):
         pass
