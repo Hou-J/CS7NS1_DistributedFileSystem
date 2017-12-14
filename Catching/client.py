8ce368c13ad0088bb51a4f814d35f79eea442422
jhou <jiongxuhou@gmail.com>
2017-12-14 21:07:11 +0000

    start doing catching based on access

---
+++ Catching/client.py
@@ -0,0 +1,56 @@
+from clientlibrary import clientLibrary
+import sys
+
+if (len(sys.argv) < 3):
+    print("Server usage: python Server.py [IP] [PORT]")
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    address = "{}:{}".format(sys.argv[1], int(sys.argv[2]))
+
+    while True:
+        actionNum = int(input(
+            "---------------------------------------\n"
+            "|Input nuber of what you want to do:  |\n"
+            "|1. View the file list.               |\n"
+            "|2. Read a File.                      |\n"
+            "|3. Add a new file.                   |\n"
+            "|4. Edit a file.                      |\n"
+            "|5. Delete a file                     |\n"
+            "|                                     |\n"
+            "|0. Exit.                             |\n"
+            "---------------------------------------\n"))
+
+        if actionNum == 1:
+            clientLibrary.fileLists(clientLibrary, address)
+
+        elif actionNum == 2:
+            fileToOpen = input("Input the complete filename you want to open: ")
+            clientLibrary.readFile(clientLibrary, address, fileToOpen)
+
+        elif actionNum == 3:
+            fileToAdd = input("Input the complete filename you want to add: ")
+            fileData = input("Input what you want to add to the {}: ".format(fileToAdd))
+            clientLibrary.addFile(clientLibrary, address, fileToAdd, fileData)
+
+        elif actionNum == 4:
+            fileToAdd = input("Input the complete filename you want to edit: ")
+            fileData = input("Input the new data in {}: ".format(fileToAdd))
+            clientLibrary.editFile(clientLibrary, address, fileToAdd, fileData)
+
+        elif actionNum == 5:
+            fileToDelete = input("Input the complete filename you want to delete: ")
+            yn = input("Are you sure you want to delete {} ? (Y/N)".format(fileToDelete))
+            if yn == "n" or yn == "N":
+                continue
+            clientLibrary.deleteFile(clientLibrary, address, fileToDelete)
+
+
+        elif actionNum == 0:
+            break
+
+        else:
+            print("Wrong Input!")
+
+        input("Press enter to continue")
---
+++ Catching/server.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import os, sys
+
+if (len(sys.argv) < 2):
+    print("Server usage: python Server.py [PORT]")
+    sys.exit(0)
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class serverFileList(Resource):
+    def get(self):
+        fileslist = []
+        files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+        print(files_path)
+        for filename in os.listdir(files_path):
+            print(filename)
+            fileslist.append(filename)
+        print(fileslist)
+        return fileslist
+
+    def post(self):
+        r = reqparse.RequestParser()
+        r.add_argument('fileName', type=str, location='json')
+        r.add_argument('data', type=str, location='json')
+        print(r.parse_args()['fileName'], "to add.")
+        filename = r.parse_args()['fileName']
+        files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+        f = [f for f in fileslist if f == filename]
+        if len(f) != 0:
+            return False
+        fileslist.append(filename)
+        addFilePath = os.path.join(files_path, filename)
+        print(addFilePath)
+        addFile = open(addFilePath, 'w')
+        addFile.write(r.parse_args()['data'])
+        addFile.close()
+        with open(os.path.join(files_path, filename)) as f:
+            data = f.readlines()
+        return data
+
+
+class serverfile(Resource):
+    def get(self, filename):
+        files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+        f = [f for f in fileslist if f == filename]
+        if len(f) == 0:
+            return False
+        with open(os.path.join(files_path, filename)) as f:
+            data = f.readlines()
+        return data
+
+    def put(self, filename):
+        r = reqparse.RequestParser()
+        r.add_argument('data', type=str, location='json')
+        files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+        f = [f for f in fileslist if f == filename]
+        if len(f) == 0:
+            return False
+        editFilePath = os.path.join(files_path, filename)
+        print(editFilePath)
+        currentFile = open(editFilePath, 'w')
+        currentFile.write(r.parse_args()['data'])
+        currentFile.close()
+        with open(os.path.join(files_path, filename)) as f:
+            data = f.readlines()
+        return data
+
+    def delete(self, filename):
+        files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+        f = [f for f in fileslist if f == filename]
+        if len(f) == 0:
+            return False
+        deleteFilePath = os.path.join(files_path, filename)
+        print(deleteFilePath)
+        os.remove(deleteFilePath)
+        fileslist.remove(fileslist.index(filename))
+        return True
+
+
+api.add_resource(serverFileList, '/fileList')
+api.add_resource(serverfile, '/file/<string:filename>')
+
+if __name__ == '__main__':
+    files_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files")
+    fileslist = []
+    for filename in os.listdir(files_path):
+        print(filename)
+        fileslist.append(filename)
+
+    app.run(host="0.0.0.0", port=int(sys.argv[1]))
